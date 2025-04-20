59626aa609dcc886dacc3668f95ac514f59bc58c
fatisssa <fm119041@stud.ur.edu.pl>
2025-04-20 15:30:30 +0200

    production version v1

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ load_dotenv()
 
 client = OpenAI(
   base_url="https://openrouter.ai/api/v1",
-  api_key=os.getenv("OPENROUTER_API_KEY")
+  api_key=st.secrets["OPENROUTER_API_KEY"]
 )
 
 completion = client.chat.completions.create(
