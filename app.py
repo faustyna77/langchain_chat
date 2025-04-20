3468d88298601e7c935f92269dbb08d3e46a73c1
fatisssa <fm119041@stud.ur.edu.pl>
2025-04-20 15:37:11 +0200

    v2

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from openai import OpenAI
-from dotenv import load_dotenv
+
 from pprint import pprint
 import os
 import json 
@@ -8,29 +8,16 @@ import streamlit as st
 st.set_page_config(page_title="OpenRouter API", page_icon=":guardsman:", layout="wide")
 st.title("Mój osobisty Chatbot ")
 
-load_dotenv()
+
 
 
 
 client = OpenAI(
   base_url="https://openrouter.ai/api/v1",
-  api_key=st.secrets["OPENROUTER_API_KEY"]
+  api_key=st.secrets("OPENROUTER_API_KEY")
 )
 
-completion = client.chat.completions.create(
-  extra_headers={
-    "HTTP-Referer": "fastinatechnology.streamlit.app", # Optional. Site URL for rankings on openrouter.ai.
-    "X-Title": "Faustyna Misiura", # Optional. Site title for rankings on openrouter.ai.
-  },
-  extra_body={},
-  model="thudm/glm-z1-32b:free",
-  messages=[
-    {
-      "role": "user",
-      "content": "Wat is your name?"
-    }
-  ]
-)
+
 
 def generate_response(prompt):
     
