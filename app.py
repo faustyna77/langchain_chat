d23dedca281ff0ed898c163b93d860ae58cdb7d4
fatisssa <fm119041@stud.ur.edu.pl>
2025-04-20 15:22:35 +0200

    chatbot version 1

---
+++ app.py
@@ -0,0 +1,66 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+from pprint import pprint
+import os
+import json 
+import streamlit as st
+
+st.set_page_config(page_title="OpenRouter API", page_icon=":guardsman:", layout="wide")
+st.title("My personnaly assistant to raports creating")
+
+load_dotenv()
+
+
+
+client = OpenAI(
+  base_url="https://openrouter.ai/api/v1",
+  api_key=os.getenv("OPENROUTER_API_KEY")
+)
+
+completion = client.chat.completions.create(
+  extra_headers={
+    "HTTP-Referer": "fastinatechnology.streamlit.app", # Optional. Site URL for rankings on openrouter.ai.
+    "X-Title": "Faustyna Misiura", # Optional. Site title for rankings on openrouter.ai.
+  },
+  extra_body={},
+  model="thudm/glm-z1-32b:free",
+  messages=[
+    {
+      "role": "user",
+      "content": "Wat is your name?"
+    }
+  ]
+)
+
+def generate_response(prompt):
+    
+
+    client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=os.getenv("OPENROUTER_API_KEY")
+    )
+
+    completion = client.chat.completions.create(
+    extra_headers={
+        "HTTP-Referer": "fastinatechnology.streamlit.app", # Optional. Site URL for rankings on openrouter.ai.
+        "X-Title": "Faustyna Misiura", # Optional. Site title for rankings on openrouter.ai.
+    },
+    extra_body={},
+    model="thudm/glm-z1-32b:free",
+    messages=[
+        {
+        "role": "user",
+        "content": prompt
+        }
+    ]
+    )
+    
+    return (completion.choices[0].message.content)
+
+
+prompt=st.text_input("Enter your prompt here")
+if st.button("Generuj odpowiedź"):
+    response = generate_response(prompt)
+    st.write(response)
+
+
